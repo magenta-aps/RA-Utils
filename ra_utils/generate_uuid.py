f8e88f7065b1b523d16a4f578e0f505d06088602
Benjamin Lyng Jørgsen <blj@magenta-aps.dk>
2021-05-28 11:23:29 +0000

    Apply 3 suggestion(s) to 2 file(s)

--- ra_utils/generate_uuid.py
+++ ra_utils/generate_uuid.py
@@ -4,28 +4,42 @@
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
 import hashlib
-import uuid
 from functools import lru_cache
 from functools import partial
 from typing import Callable
+from uuid import UUID
 
 
 @lru_cache(maxsize=None)
-def generate_uuid(base: str, value: str) -> uuid.UUID:
+def _generate_uuid(value: str) -> UUID:
+    value_hash = hashlib.md5(value.encode())
+    value_digest = value_hash.hexdigest()
+    return UUID(value_digest)
+
+
+def generate_uuid(base: str, value: str) -> UUID:
     """
     Generate a predictable uuid based on org name and a unique value.
     """
-    base_hash = hashlib.md5(base.encode())
-    base_digest = base_hash.hexdigest()
-    base_uuid = uuid.UUID(base_digest)
-
-    combined_value = (str(base_uuid) + str(value)).encode()
-    value_hash = hashlib.md5(combined_value)
-    value_digest = value_hash.hexdigest()
-    value_uuid = uuid.UUID(value_digest)
-    return value_uuid
+    base_uuid = _generate_uuid(base)
+    return _generate_uuid(str(base_uuid) + str(value))
 
 
-def uuid_generator(base: str) -> Callable:
-    """Make a uuid generator with a fixed base"""
+def uuid_generator(base: str) -> Callable[[str], UUID]:
+    """Make an UUID generator with a fixed base/seed.
+
+    Example:
+        uuid_gen = uuid_generator("secret_seed")
+        uuid1 = uuid_gen("facetnavn1")
+        uuid2 = uuid_gen("facetnavn1")
+        uuid3 = uuid_gen("facetnavn2")
+        assert uuid1 == uuid2
+        assert uuid1 != uuid3
+
+    Args:
+        base (str): Base/seed utilized for all calls to generator.
+
+    Returns:
+        Callable: Function to map strings to UUIDs.
+    """
     return partial(generate_uuid, base)
--- tests/test_generate_uuid.py
+++ tests/test_generate_uuid.py
@@ -9,24 +9,32 @@ from uuid import UUID
 from hypothesis import given
 from hypothesis.strategies import text
 
+from ra_utils.generate_uuid import _generate_uuid
 from ra_utils.generate_uuid import generate_uuid
 from ra_utils.generate_uuid import uuid_generator
 
 
 class test_generate_uuid(TestCase):
     @given(text(), text())
-    def test_generate_uuid(self, value, base):
-        uuid1 = generate_uuid(value, base)
-        uuid2 = generate_uuid(value, base)
-        uuid3 = generate_uuid(value, base + "A different string")
+    def test_generate_uuid(self, base, value):
+        uuid1 = generate_uuid(base, value)
+        _generate_uuid.cache_clear()
+        uuid2 = generate_uuid(base, value)
+        _generate_uuid.cache_clear()
+        uuid3 = generate_uuid(base, value + "A different string")
         assert uuid1 == uuid2
         assert uuid1 != uuid3
         assert isinstance(uuid1, UUID)
 
     @given(text(), text())
-    def test_create_generator(self, value, base):
+    def test_create_generator(self, base, value):
         gen = uuid_generator(base)
         uuid1 = gen(value)
-        assert uuid1 == gen(value)
-        assert uuid_generator != gen(value + "Another string")
+        uuid2 = gen(value)
+        assert uuid1 == uuid2
+        assert uuid_generator(value) != gen(value + "Another string")
         assert isinstance(uuid1, UUID)
+
+    @given(text(), text())
+    def test_generator_output(self, base, value):
+        assert generate_uuid(base, value) == uuid_generator(base)(value)
