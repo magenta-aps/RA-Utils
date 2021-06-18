b5e4675dcadf00a6ba107cf1f38969c6f47420c0
Emil Madsen <sovende@gmail.com>
2021-06-18 19:24:48 +0200

    Documentation and cleanup tests

--- ra_utils/dict_map.py
+++ ra_utils/dict_map.py
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
+from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Optional
@@ -12,8 +13,8 @@ from more_itertools import unzip
 
 def dict_map(
     dicty: Dict,
-    key_func: Optional[Callable] = None,
-    value_func: Optional[Callable] = None,
+    key_func: Optional[Callable[[Any], Any]] = None,
+    value_func: Optional[Callable[[Any], Any]] = None,
 ) -> Dict:
     """Map a dictionary's keys and values.
 
@@ -30,7 +31,7 @@ def dict_map(
         one another, i.e. the key_function should be a bijective function.
 
     Returns:
-        dict: A dict where func has been applied to every value.
+        dict: A dict where the functions has been applied to their values.
     """
     # Handle base-cases, i.e. empty dict and no transformation
     if not dicty:
@@ -44,3 +45,37 @@ def dict_map(
     if value_func:
         values = map(value_func, values)
     return dict(zip(keys, values))
+
+
+def dict_map_key(key_func: Callable[[Any], Any], dicty: Dict) -> Dict:
+    """Map a dictionary's keys.
+
+    Example:
+        pow2 = lambda x: x**2
+        input_dict = {1: 1, 2: 2, 3: 3}
+        output_dict = dict_map_key(pow2, input_dict)
+        self.assertEqual(output_dict, {1: 1, 4: 2, 9: 3})
+
+    Note:
+        Care must be taken for mapped keys to not destructively interfere with
+        one another, i.e. the key_function should be a bijective function.
+
+    Returns:
+        dict: A dict where key_func has been applied to every key.
+    """
+    return dict_map(dicty, key_func=key_func)
+
+
+def dict_map_value(value_func: Callable[[Any], Any], dicty: Dict) -> Dict:
+    """Map a dictionary's values.
+
+    Example:
+        pow2 = lambda x: x**2
+        input_dict = {1: 1, 2: 2, 3: 3}
+        output_dict = dict_map_value(pow2, input_dict)
+        self.assertEqual(output_dict, {1: 1, 2: 4, 3: 9})
+
+    Returns:
+        dict: A dict where value_func has been applied to every value.
+    """
+    return dict_map(dicty, value_func=value_func)
--- tests/test_dict_map.py
+++ tests/test_dict_map.py
@@ -11,7 +11,9 @@ from hypothesis import assume
 from hypothesis import given
 from hypothesis import strategies as st
 
-from ra_utils.transpose_dict import dict_map
+from ra_utils.dict_map import dict_map
+from ra_utils.dict_map import dict_map_key
+from ra_utils.dict_map import dict_map_value
 
 
 def swapcase(x):
@@ -31,7 +33,7 @@ def pow2(x):
         partial(dict_map, key_func=swapcase, value_func=swapcase),
     ],
 )
-def test_dictmap_identity(dict_map_func):
+def test_dictmap_emptydict(dict_map_func):
     dicty: Dict[str, str] = {}
     mapped = dict_map_func(dicty)
     assert id(mapped) == id(dicty)
@@ -39,44 +41,45 @@ def test_dictmap_identity(dict_map_func):
 
 
 @given(st.dictionaries(st.text(), st.text(), min_size=1))
-def test_dictmap_swapcase(dicty):
+def test_dictmap_identity(dicty):
     mapped = dict_map(dicty)
     assert id(mapped) == id(dicty)
     assert dict_map(mapped) == dicty
 
-    mapped = dict_map(
-        dicty,
-        key_func=swapcase,
-    )
+
+@given(st.dictionaries(st.text(), st.text(), min_size=1))
+def test_dictmap_swapcase_key(dicty):
+    mapped = dict_map(dicty, key_func=swapcase)
     assert id(mapped) != id(dicty)
     assert sorted(mapped.keys()) == sorted(map(swapcase, dicty.keys()))
     assert sorted(mapped.values()) == sorted(dicty.values())
 
-    mapped = dict_map(
-        dicty,
-        value_func=swapcase,
-    )
+    key_mapped = dict_map_key(swapcase, dicty)
+    assert key_mapped == mapped
+
+
+@given(st.dictionaries(st.text(), st.text(), min_size=1))
+def test_dictmap_swapcase_value(dicty):
+    mapped = dict_map(dicty, value_func=swapcase)
     assert id(mapped) != id(dicty)
     assert sorted(mapped.keys()) == sorted(dicty.keys())
     assert sorted(mapped.values()) == sorted(map(swapcase, dicty.values()))
 
-    mapped = dict_map(
-        dicty,
-        key_func=swapcase,
-        value_func=swapcase,
-    )
+    value_mapped = dict_map_value(swapcase, dicty)
+    assert value_mapped == mapped
+
+
+@given(st.dictionaries(st.text(), st.text(), min_size=1))
+def test_dictmap_swapcase_key_value(dicty):
+    mapped = dict_map(dicty, key_func=swapcase, value_func=swapcase)
     assert id(mapped) != id(dicty)
     assert sorted(mapped.keys()) == sorted(map(swapcase, dicty.keys()))
     assert sorted(mapped.values()) == sorted(map(swapcase, dicty.values()))
 
 
 @given(st.dictionaries(st.text(), st.integers(), min_size=1))
-def test_dictmap_pow2(dicty):
-    mapped = dict_map(
-        dicty,
-        key_func=swapcase,
-        value_func=pow2,
-    )
+def test_dictmap_pow2_key_value(dicty):
+    mapped = dict_map(dicty, key_func=swapcase, value_func=pow2)
     assert id(mapped) != id(dicty)
     assert sorted(mapped.keys()) == sorted(map(swapcase, dicty.keys()))
     assert sorted(mapped.values()) == sorted(map(pow2, dicty.values()))
@@ -84,12 +87,19 @@ def test_dictmap_pow2(dicty):
 
 @given(st.dictionaries(st.text(), st.text(), min_size=1))
 def test_dictmap_invalid_operation(dicty):
+    pow2_error = "unsupported operand type(s) for ** or pow()"
+
     with pytest.raises(TypeError) as exc_info:
-        dict_map(
-            dicty,
-            key_func=pow2,
-        )
-    assert "unsupported operand type(s) for ** or pow():" in str(exc_info.value)
+        dict_map(dicty, key_func=pow2)
+    assert pow2_error in str(exc_info.value)
+
+    with pytest.raises(TypeError) as exc_info:
+        dict_map_key(pow2, dicty)
+    assert pow2_error in str(exc_info.value)
+
+    with pytest.raises(TypeError) as exc_info:
+        dict_map_value(pow2, dicty)
+    assert pow2_error in str(exc_info.value)
 
 
 @given(st.text(min_size=1))
@@ -101,12 +111,12 @@ def test_dict_map_destructive_key_interference(key):
     assume(key != to_upper(key))
 
     dicty = {key: 0, to_upper(key): 1}
-    mapped = dict_map(
-        dicty,
-        key_func=to_upper,
-    )
+    mapped = dict_map(dicty, key_func=to_upper)
     assert id(mapped) != id(dicty)
     assert len(mapped.keys()) == 1
     assert len(mapped.values()) == 1
     assert len(dicty.keys()) == 2
     assert len(dicty.values()) == 2
+
+    key_mapped = dict_map_key(to_upper, dicty)
+    assert key_mapped == mapped
