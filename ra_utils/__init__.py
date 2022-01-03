c5e7cca7a7ed0a217b6b4f0b042c82abcf1226e2
Nina Jensen <nkj@magenta-aps.dk>
2022-01-03 10:06:18 +0100

    Remove version & test

--- ra_utils/__init__.py
+++ ra_utils/__init__.py
@@ -3,6 +3,3 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
-from .semantic_version_type import SemanticVersion
-
-__version__: SemanticVersion = SemanticVersion("0.4.0")
