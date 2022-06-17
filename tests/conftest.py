96b749320e61421bf24556a6c71ac3f9d9f7c4f8
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-06-17 13:24:49 +0200

    Add sentry_init method

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,4 +11,4 @@ from hypothesis import settings
 settings.register_profile("ci", max_examples=1000, deadline=None)
 settings.register_profile("deep", max_examples=10000, deadline=None)
 settings.register_profile("dev", max_examples=1000, deadline=None)
-settings.load_profile(os.getenv(u"HYPOTHESIS_PROFILE", "dev"))
+settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "dev"))
