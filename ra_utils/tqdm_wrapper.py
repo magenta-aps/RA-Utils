162a4dac9f6b8b763cf334ca27e84ff8810dbdfe
Mads Riis-Weitling <mrw@magenta.dk>
2022-12-02 16:14:03 +0100

    chore: [#53614] Add link to relevant `tqdm` doc

--- ra_utils/tqdm_wrapper.py
+++ ra_utils/tqdm_wrapper.py
@@ -8,6 +8,7 @@ from tqdm import tqdm as _tqdm
 def tqdm(iterable: Any = None, **kwargs: Any) -> _tqdm:
     """Wrap `tqdm`, ensuring that we only print progress bars in interactive sessions,
     and *not* when running from cron or other non-interactive environments.
+    # See: https://tqdm.github.io/docs/tqdm/#__init__
     """
     disable: bool = kwargs.pop("disable", None)
     return _tqdm(iterable=iterable, disable=disable, **kwargs)
