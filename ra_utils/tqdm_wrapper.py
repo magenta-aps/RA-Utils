27a5ef3cf8d06ec791c45480a8504aaa80ce2052
Mads Riis-Weitling <mrw@magenta.dk>
2022-12-02 15:42:22 +0100

    feat: [#53614] Add `ra_utils.tqdm_wrapper`
    This extracts the code in
    https://git.magenta.dk/rammearkitektur/os2mo-data-import-and-export/-/blob/master/helpers/__init__.py
    from DIPEX to `ra-utils`

---
+++ ra_utils/tqdm_wrapper.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from typing import Any
+
+from tqdm import tqdm as _tqdm
+
+
+def tqdm(iterable: Any = None, **kwargs: Any) -> _tqdm:
+    """Wrap `tqdm`, ensuring that we only print progress bars in interactive sessions,
+    and *not* when running from cron or other non-interactive environments.
+    """
+    disable: bool = kwargs.pop("disable", None)
+    return _tqdm(iterable=iterable, disable=disable, **kwargs)
