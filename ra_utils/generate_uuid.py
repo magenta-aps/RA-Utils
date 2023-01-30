0e5ac216f047d21b7a50017083c21e06f69b4c5d
Emil Madsen <sovende@gmail.com>
2023-01-30 15:28:59 +0100

    chore: [#52218] remove #! header from non-executable files

--- ra_utils/generate_uuid.py
+++ ra_utils/generate_uuid.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # --------------------------------------------------------------------------------------
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
