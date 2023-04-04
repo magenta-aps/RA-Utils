a34d14d732e703714bdd734effe044586dff44c5
Carl Bordum Hansen <carl@bordum.dk>
2023-04-04 13:46:11 +0200

    refactor: check_docs.py

--- tools/check_docs.py
+++ tools/check_docs.py
@@ -3,35 +3,25 @@
 # SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 # --------------------------------------------------------------------------------------
-from os import listdir
-from os.path import isfile
-from os.path import join
+from glob import glob
 from pathlib import Path
-from typing import Set
 
 import yaml
-from more_itertools import flatten
-from more_itertools import one
-
-
-def files_in_folder(directory: str) -> Set[str]:
-    dir_contents = listdir(directory)
-    dir_files = filter(lambda entry: isfile(join(directory, entry)), dir_contents)
-    return set(dir_files)
 
 
 python_directory = "ra_utils"
 markdown_directory = "docs/modules"
 
-python_files = files_in_folder(python_directory)
-python_files.remove("__init__.py")
-python_files.remove("py.typed")
+
+python_files = set(glob("*.py", root_dir=python_directory, recursive=True)) - {
+    "__init__.py",
+}
+markdown_files = set(glob("*.md", root_dir=markdown_directory, recursive=True))
+
 
 # We want a markdown file for each python file
-wanted_markdown_files = set(map(lambda file: file.replace(".py", ".md"), python_files))
-
-actual_markdown_files = files_in_folder(markdown_directory)
-mismatch = wanted_markdown_files.symmetric_difference(actual_markdown_files)
+wanted_markdown_files = {file.replace(".py", ".md") for file in python_files}
+mismatch = wanted_markdown_files ^ markdown_files
 if mismatch:
     print("File mismatch between python code and module docs", mismatch)
     exit(1)
@@ -40,16 +30,10 @@ if mismatch:
 mkdocs_text = Path("mkdocs.yml").read_text()
 mkdocs = yaml.safe_load(mkdocs_text)
 nav = mkdocs["nav"]
-dict_entries = filter(lambda entry: isinstance(entry, dict), nav)
-dict_entries = filter(lambda entry: entry.keys() == {"Modules"}, dict_entries)
-modules = one(dict_entries)["Modules"]
-modules = flatten(map(lambda entry: entry.values(), modules))  # type: ignore
-modules = set(map(lambda entry: entry[len("modules/") :], modules))  # type: ignore
-# At this point modules is a set of filenames in the TOC
+modules = next(e["Modules"] for e in nav if isinstance(e, dict) and "Modules" in e)
+modules_in_toc = {next(iter(m.values())).removeprefix("modules/") for m in modules}
 
-mismatch = modules.symmetric_difference(actual_markdown_files)
+mismatch = modules_in_toc ^ markdown_files
 if mismatch:
     print("Mismatch between module docs and index", mismatch)
     exit(1)
-
-exit(0)
